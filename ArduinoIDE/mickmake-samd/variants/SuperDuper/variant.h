a8474ea11f50158e40a9ae4ee51b14e1cfbbd1d4
Mick Hellstrom <mick@macmini.homenet>
2018-04-12 10:14:43 +1000

    Version 1.0.1

---
+++ ArduinoIDE/mickmake-samd/variants/SuperDuper/variant.h
@@ -0,0 +1,237 @@
+/*
+  Copyright (c) 2014-2015 Arduino LLC.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef _VARIANT_ARDUINO_ZERO_
+#define _VARIANT_ARDUINO_ZERO_
+
+// The definitions here needs a SAMD core >=1.6.10
+#define ARDUINO_SAMD_VARIANT_COMPLIANCE 10610
+
+/*----------------------------------------------------------------------------
+ *        Definitions
+ *----------------------------------------------------------------------------*/
+
+/** Frequency of the board main oscillator */
+#define VARIANT_MAINOSC		(32768ul)
+
+/** Master clock frequency */
+#define VARIANT_MCK			  (48000000ul)
+
+/*----------------------------------------------------------------------------
+ *        Headers
+ *----------------------------------------------------------------------------*/
+
+#include "WVariant.h"
+
+#ifdef __cplusplus
+#include "SERCOM.h"
+#include "Uart.h"
+#endif // __cplusplus
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif // __cplusplus
+
+/*----------------------------------------------------------------------------
+ *        Pins
+ *----------------------------------------------------------------------------*/
+
+// Number of pins defined in PinDescription array
+#define PINS_COUNT           (35u)
+#define NUM_DIGITAL_PINS     (14u)
+#define NUM_ANALOG_INPUTS    (1u)
+#define NUM_ANALOG_OUTPUTS   (0u)
+#define analogInputToDigitalPin(p)  ((p < 6u) ? (p) + 14u : -1)
+
+#define digitalPinToPort(P)        ( &(PORT->Group[g_APinDescription[P].ulPort]) )
+#define digitalPinToBitMask(P)     ( 1 << g_APinDescription[P].ulPin )
+//#define analogInPinToBit(P)        ( )
+#define portOutputRegister(port)   ( &(port->OUT.reg) )
+#define portInputRegister(port)    ( &(port->IN.reg) )
+#define portModeRegister(port)     ( &(port->DIR.reg) )
+#define digitalPinHasPWM(P)        ( g_APinDescription[P].ulPWMChannel != NOT_ON_PWM || g_APinDescription[P].ulTCChannel != NOT_ON_TIMER )
+
+/*
+ * digitalPinToTimer(..) is AVR-specific and is not defined for SAMD
+ * architecture. If you need to check if a pin supports PWM you must
+ * use digitalPinHasPWM(..).
+ *
+ * https://github.com/arduino/Arduino/issues/1833
+ */
+// #define digitalPinToTimer(P)
+
+// LEDs
+#define LED_GREEN	(16u)
+#define LED_BLUE	(17u)
+#define PIN_LED_13	LED_GREEN
+#define PIN_LED_RXL	LED_GREEN
+#define PIN_LED_TXL	LED_BLUE
+#define PIN_LED		LED_GREEN
+#define PIN_LED2	PIN_LED_RXL
+#define PIN_LED3	PIN_LED_TXL
+#define LED_BUILTIN	LED_GREEN
+
+// Other GPIO
+#define BUZZER		(6ul)
+#define BUTTON1		(2ul)
+#define BUTTON2		(3ul)
+#define BUTTON3		(4ul)
+#define BUTTON4		(5ul)
+
+
+// Analog pins
+#define BATTERY		(18ul)
+#define PIN_A0		BATTERY
+#define PIN_DAC0	BATTERY
+
+static const uint8_t A0  = PIN_A0;
+static const uint8_t DAC0 = PIN_DAC0;
+
+#define ADC_RESOLUTION	12
+
+// Other pins
+#define PIN_ATN              (18ul)
+static const uint8_t ATN = PIN_ATN;
+
+// Serial interfaces
+//#define PIN_SERIAL_RX       (0ul)
+//#define PIN_SERIAL_TX       (1ul)
+//#define PAD_SERIAL_TX       (UART_TX_PAD_2)
+//#define PAD_SERIAL_RX       (SERCOM_RX_PAD_3)
+
+// Serial1
+#define PIN_SERIAL1_RX       (0ul)
+#define PIN_SERIAL1_TX       (1ul)
+#define PAD_SERIAL1_TX       (UART_TX_PAD_2)
+#define PAD_SERIAL1_RX       (SERCOM_RX_PAD_3)
+
+/*
+ * SPI Interfaces
+ */
+#define SPI_INTERFACES_COUNT 3
+
+// SPI3
+#define PIN_SPI_MISO	(21u)
+#define PIN_SPI_MOSI	(22u)
+#define PIN_SPI_SCK	(23u)
+#define PERIPH_SPI	sercom0
+#define PAD_SPI_TX	SPI_PAD_2_SCK_3
+#define PAD_SPI_RX	SERCOM_RX_PAD_0
+
+// SD1
+#define PIN_SPI1_MISO	(27u)
+#define PIN_SPI1_MOSI	(29u)
+#define PIN_SPI1_SCK	(30u)
+#define PERIPH_SPI1	sercom4
+#define PAD_SPI1_TX	SPI_PAD_2_SCK_3
+#define PAD_SPI1_RX	SERCOM_RX_PAD_0
+
+// SD2
+#define PIN_SPI2_MISO	(31u)
+#define PIN_SPI2_MOSI	(33u)
+#define PIN_SPI2_SCK	(34u)
+#define PERIPH_SPI2	sercom2
+#define PAD_SPI2_TX	SPI_PAD_2_SCK_3
+#define PAD_SPI2_RX	SERCOM_RX_PAD_0
+
+
+static const uint8_t SS	  = PIN_A0 ;	// SERCOM4 last PAD is present on A2 but HW SS isn't used. Set here only for reference.
+static const uint8_t MOSI = PIN_SPI_MOSI ;
+static const uint8_t MISO = PIN_SPI_MISO ;
+static const uint8_t SCK  = PIN_SPI_SCK ;
+
+/*
+ * Wire Interfaces
+ */
+#define WIRE_INTERFACES_COUNT 1
+
+#define PIN_WIRE_SDA         (19u)
+#define PIN_WIRE_SCL         (20u)
+#define PERIPH_WIRE          sercom1
+#define WIRE_IT_HANDLER      SERCOM1_Handler
+
+static const uint8_t SDA = PIN_WIRE_SDA;
+static const uint8_t SCL = PIN_WIRE_SCL;
+
+/*
+ * USB
+ */
+#define PIN_USB_HOST_ENABLE (24ul)
+#define PIN_USB_DM          (25ul)
+#define PIN_USB_DP          (26ul)
+
+/*
+ * I2S Interfaces
+ */
+#define I2S_INTERFACES_COUNT 0
+
+#define I2S_DEVICE          0
+#define I2S_CLOCK_GENERATOR 3
+#define PIN_I2S_SD          (9u)
+#define PIN_I2S_SCK         (1u)
+#define PIN_I2S_FS          (0u)
+
+#ifdef __cplusplus
+}
+#endif
+
+/*----------------------------------------------------------------------------
+ *        Arduino objects - C++ only
+ *----------------------------------------------------------------------------*/
+
+#ifdef __cplusplus
+
+/*	=========================
+ *	===== SERCOM DEFINITION
+ *	=========================
+*/
+extern SERCOM sercom0;
+extern SERCOM sercom1;
+extern SERCOM sercom2;
+extern SERCOM sercom3;
+extern SERCOM sercom4;
+extern SERCOM sercom5;
+
+extern Uart Serial1;
+
+#endif
+
+// These serial port names are intended to allow libraries and architecture-neutral
+// sketches to automatically default to the correct port name for a particular type
+// of use.  For example, a GPS module would normally connect to SERIAL_PORT_HARDWARE_OPEN,
+// the first hardware serial port whose RX/TX pins are not dedicated to another use.
+//
+// SERIAL_PORT_MONITOR        Port which normally prints to the Arduino Serial Monitor
+//
+// SERIAL_PORT_USBVIRTUAL     Port which is USB virtual serial
+//
+// SERIAL_PORT_LINUXBRIDGE    Port which connects to a Linux system via Bridge library
+//
+// SERIAL_PORT_HARDWARE       Hardware serial port, physical RX & TX pins.
+//
+// SERIAL_PORT_HARDWARE_OPEN  Hardware serial ports which are open for use.  Their RX & TX
+//                            pins are NOT connected to anything by default.
+#define SERIAL_PORT_USBVIRTUAL      Serial
+#define SERIAL_PORT_MONITOR         Serial
+// Serial has no physical pins broken out, so it's not listed as HARDWARE port
+#define SERIAL_PORT_HARDWARE        Serial1
+#define SERIAL_PORT_HARDWARE_OPEN   Serial1
+
+#endif /* _VARIANT_ARDUINO_ZERO_ */
+
